de06c2e255bf8fb351865f11da1235557c7a4121
Eduardo K Yasuoka <129549675+YASUOKA01@users.noreply.github.com>
2023-12-08 22:04:14 -0300

    Update script.js

--- index.html/script.js
+++ index.html/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    // Verificar se há uma preferência de tema salva e aplicar
+
     const savedTheme = localStorage.getItem('darkTheme') === 'true';
     setTheme(savedTheme);
 
-    // Associar eventos aos elementos
+
     $('#toggle-theme').on('click', function () {
         const isDarkTheme = $('body').hasClass('dark-theme');
         setTheme(!isDarkTheme);
